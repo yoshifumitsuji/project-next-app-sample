069466436efecd67e020eadd52bb701384178494
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-10-21 16:31:32 +0900

    polish in underlined link 01

--- src/components/effect/Link/UnderlinedLink01/index.tsx
+++ src/components/effect/Link/UnderlinedLink01/index.tsx
@@ -2,18 +2,13 @@ import { Link } from '../../../ui/Link'
 import styles from './index.module.scss'
 
 type Props = {
-  color: string
-  blur: number
-  offsetX: number
-  offsetY: number
   children: string
 }
 
 export const UnderlinedLink01 = (props: Props) => {
   return (
     <span className={styles.link}>
-      <Link href=''>{props.children}</Link>
+      <Link href='#'>{props.children}</Link>
     </span>
   )
-  // return <a className={styles.link}>{props.children}</a>
 }
